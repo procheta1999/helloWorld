ea024cbb57a3b6b94de3ad57dc0cc51fc3fdd717
procheta1999 <bhattacharyya.procheta1999@gmail.com>
2022-01-19 21:07:43 +0530

    4th commit

--- src/hello.cpp
+++ src/hello.cpp
@@ -6,4 +6,6 @@ int main(int argc, char *argv[])
     std::cout<<"Commit 1"<<std::endl;
     std::cout<<"Commit 2"<<std::endl;
     std::cout<<"BUG123 Branch"<<std::endl;
+    std::cout<<"Commit 3"<<std::endl;
+    std::cout<<"Commit 4"<<std::endl;
 }
