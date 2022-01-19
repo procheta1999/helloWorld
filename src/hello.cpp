da8d80097ca9f43d79a527e3966d36271c493382
procheta1999 <bhattacharyya.procheta1999@gmail.com>
2022-01-19 21:03:00 +0530

    bug123 fixed

--- src/hello.cpp
+++ src/hello.cpp
@@ -5,4 +5,5 @@ int main(int argc, char *argv[])
 	std::cout << "Hello world!" << std::endl;
     std::cout<<"Commit 1"<<std::endl;
     std::cout<<"Commit 2"<<std::endl;
+    std::cout<<"BUG123 Branch"<<std::endl;
 }
