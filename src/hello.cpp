9be856c1170ddc0019f8a2f2e70cbd74743aed64
procheta1999 <bhattacharyya.procheta1999@gmail.com>
2022-01-19 21:56:50 +0530

    hello

--- src/hello.cpp
+++ src/hello.cpp
@@ -5,14 +5,5 @@ int main(int argc, char *argv[])
 	std::cout << "Hello world!" << std::endl;
     std::cout<<"Commit 1"<<std::endl;
     std::cout<<"Commit 2"<<std::endl;
-    std::cout<<"BUG123 Branch"<<std::endl;
-    std::cout<<"Commit 3"<<std::endl;
-    std::cout<<"Commit 4"<<std::endl;
-<<<<<<< HEAD
-    std::cout<<"Commit 7"<<std::endl;
-    std::cout<<"Commit 8"<<std::endl;
-=======
-    std::cout<<"Commit 5"<<std::endl;
-    std::cout<<"Commit 6"<<std::endl;
->>>>>>> bug123
+
 }
